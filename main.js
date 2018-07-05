2c8cc6f7087ad3e258a48b49ccf118b2c10127ec
Chungzilla <chung.jazmine@gmail.com>
2018-07-05 17:03:39 -0400

    Finised styling animation on pics

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
-// window.addEventListener('scroll', () => {
-// 	//listens to scroll on entire window
 
-// 	let x = document.getElementsByClassName("hidden");
-// 			//defines all hidden content to call later
-// 	let y = window.pageYOffset;
-// 			//defines current px scrolled
-		
-// 		if(y>600) {
-// 			x.classList.add('show');
-// 				//add show class to elements w/ hidden class		
-// 		}
 
-// });
-
-  // let toShop = document.getElementByTagName("button")
 
 document.getElementById("clickHere").addEventListener("click", function(){
    alert("SORRY HYPEBEAST! EVERYTHING IS SOLD OUT!!!!!!");
